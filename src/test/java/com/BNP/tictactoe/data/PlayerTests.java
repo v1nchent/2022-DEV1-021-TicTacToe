d3192c874decd6187a0e244cf00fb30ee2e66fc9
VincentV <vincent.vindevogel@hi10.be>
2022-04-15 11:52:29 +0200

    player model test written

--- src/test/java/com/BNP/tictactoe/data/PlayerTests.java
+++ src/test/java/com/BNP/tictactoe/data/PlayerTests.java
@@ -1,4 +1,41 @@
 package com.BNP.tictactoe.data;
 
+import org.junit.jupiter.api.Test;
+
+
+import java.lang.reflect.Field;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
 public class PlayerTests {
+
+    @Test
+    public void testUserNameSetter_SetsProperly() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Player player = new Player();
+
+        //when
+        player.setUsername("Player 1");
+
+        //then
+        final Field field = player.getClass().getDeclaredField("username");
+        field.setAccessible(true);
+        assertEquals("Fields didn't match", field.get(player), "Player 1");
+    }
+
+    @Test
+    public void testUserNameGetter_GetsUsername() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Player player = new Player();
+        final Field field = player.getClass().getDeclaredField("userName");
+        field.setAccessible(true);
+        field.set(player, "Player 1");
+
+        //when
+        final String result = player.getUsername();
+
+        //then
+        assertEquals("Fields wasn't retrieved properly", result, "Player 1");
+    }
+
 }
