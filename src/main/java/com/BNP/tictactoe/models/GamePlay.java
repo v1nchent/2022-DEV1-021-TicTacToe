672751763aac342af1245c01b91cbf1247c1a92a
VincentV <vincent.vindevogel@hi10.be>
2022-04-22 16:51:24 +0200

    gameplay data added

--- src/main/java/com/BNP/tictactoe/models/GamePlay.java
+++ src/main/java/com/BNP/tictactoe/models/GamePlay.java
@@ -1,4 +1,11 @@
 package com.BNP.tictactoe.models;
 
+import lombok.Data;
+
+@Data
 public class GamePlay {
+    private TicTacToe type;
+    private Integer coordinateX;
+    private Integer coordinateY;
+    private String gameId;
 }
