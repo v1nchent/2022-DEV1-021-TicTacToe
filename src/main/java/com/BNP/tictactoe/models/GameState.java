4b4053b234ccf096cfa0970b177de53f53bd4ef2
VincentV <vincent.vindevogel@hi10.be>
2022-04-22 16:52:24 +0200

    TicTacToe added

--- src/main/java/com/BNP/tictactoe/models/GameState.java
+++ src/main/java/com/BNP/tictactoe/models/GameState.java
@@ -1,4 +1,5 @@
 package com.BNP.tictactoe.models;
 
 public enum GameState {
+    NEW, IN_PROGRESS, FINISHED
 }
--- src/main/java/com/BNP/tictactoe/models/TicTacToe.java
+++ src/main/java/com/BNP/tictactoe/models/TicTacToe.java
@@ -1,4 +1,12 @@
 package com.BNP.tictactoe.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum TicTacToe {
+    X(1), O(2);
+
+    private Integer value;
 }
