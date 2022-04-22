4c4a7c41077db7cc73d96f886f53ce94e8c8a232
VincentV <vincent.vindevogel@hi10.be>
2022-04-22 16:51:01 +0200

    game data added

--- src/main/java/com/BNP/tictactoe/models/Game.java
+++ src/main/java/com/BNP/tictactoe/models/Game.java
@@ -4,4 +4,12 @@ import lombok.Data;
 
 @Data
 public class Game {
+
+    private String gameId;
+    private Player player1;
+    private Player player2;
+    private GameState state;
+    private int[][] board;
+    private TicTacToe winner;
+
 }
