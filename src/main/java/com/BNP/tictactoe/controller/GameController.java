b60ea1213b998024bf687be78e91c17c47b2c15f
VincentV <vincent.vindevogel@hi10.be>
2022-04-26 17:01:56 +0200

    rebase prep

--- src/main/java/com/BNP/tictactoe/controller/GameController.java
+++ src/main/java/com/BNP/tictactoe/controller/GameController.java
@@ -5,29 +5,27 @@ import com.BNP.tictactoe.exception.NotFoundException;
 import com.BNP.tictactoe.models.Game;
 import com.BNP.tictactoe.models.GamePlay;
 import com.BNP.tictactoe.models.Player;
+import com.BNP.tictactoe.models.PlayerHolder;
 import com.BNP.tictactoe.service.GameService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 @AllArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/game")
 public class GameController {
 
     private final GameService gameService;
 
     @PostMapping("/start")
-    public ResponseEntity<Game> start(@RequestBody Player player1, Player player2) {
-        log.info("start game request: {} {}", player1, player2);
-        return ResponseEntity.ok(gameService.createGame(player1, player2));
+    public ResponseEntity<Game> start(@RequestBody PlayerHolder ph) {
+        log.info("start game request: {} {}", ph.getP1(), ph.getP2());
+        return ResponseEntity.ok(gameService.createGame(ph.getP1(), ph.getP2()));
     }
-
     @PostMapping("/gameplay")
     public ResponseEntity<Game> gamePlay(@RequestBody GamePlay request) throws NotFoundException, InvalidGameException {
         log.info("gameplay: {}", request);
---
+++ src/main/java/com/BNP/tictactoe/models/PlayerHolder.java
@@ -0,0 +1,9 @@
+package com.BNP.tictactoe.models;
+
+import lombok.Data;
+
+@Data
+public class PlayerHolder {
+    private Player p1;
+    private Player p2;
+}
